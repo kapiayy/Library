b3e0af1170bd961279193c775436b990a418fdef
Kapi <kacperkozlowskii12@gmail.com>
2025-05-30 23:07:00 +0200

    Add Change and Remove Button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = []
+let myLibrary = []
 
 function Book(title,author,pages,read){
      if (!new.target) {
@@ -8,10 +8,8 @@ function Book(title,author,pages,read){
     this.author = author
     this.pages = pages 
     this.id = crypto.randomUUID()
-    if(read == true){
-        this.read = 'Yes'
-    }else this.read = 'No'
     
+    this.read = read ? 'Yes' : 'No';
 }
 
 
@@ -20,25 +18,44 @@ function addBookToLibrary(title,author,pages = 0,read){
     myLibrary.push(new Book(title,author,pages,read))
 }
 
-addBookToLibrary('Hobbit','Nigger',150,true)
-addBookToLibrary('officer',`cantbreath`,69,false)
+
+
+
+
 
 console.log(myLibrary)
 
 
 
-  const container = document.querySelector(".container")
+const container = document.querySelector(".container")
 
 const bookContainer = document.createElement("div")
 
+const addBook = document.querySelector(".add-book")
+const dialog = document.querySelector("dialog")
+
+addBook.addEventListener("click", () =>{
+    dialog.showModal();
+    dialog.classList.add('show');
+    
+})
 container.appendChild(bookContainer)
-myLibrary.forEach((element) => {
+function displayBooks(){
+    
+    bookContainer.innerHTML = ''
+    myLibrary.forEach((element) => {
+        
     const BookInfo = document.createElement("div")
     const Title = document.createElement("div")
     const Author = document.createElement("div")
     const Pages = document.createElement("div")
     const Read = document.createElement("div")
-  
+    const removeButton = document.createElement("button")
+    const changeReadButton = document.createElement("button")
+    changeReadButton.textContent = 'Read'
+    removeButton.textContent = 'Remove'
+    changeReadButton.classList.add("read-button")
+    removeButton.classList.add("remove-button")
     
     Title.classList.add("title")
     Author.classList.add("author")
@@ -51,12 +68,42 @@ myLibrary.forEach((element) => {
     
     
     bookContainer.appendChild(BookInfo)
-    BookInfo.append(Author,Title,Pages,Read)
+    BookInfo.append(Author,Title,Pages,Read,removeButton,changeReadButton)
     
     Author.textContent = element.author
     Title.textContent = element.title
     Pages.textContent = element.pages
     Read.textContent = element.read
     
+    removeButton.addEventListener("click", () => {
+    myLibrary =myLibrary.filter(book => book.id != element.id)
+    displayBooks()
+})
+    changeReadButton.addEventListener("click", () => {
+        element.read = element.read === 'Yes' ? 'No' : 'Yes'
+        displayBooks()
+    })
 })
 
+}
+
+
+
+
+const addInputsButton = document.getElementById('dialog-button')
+
+addInputsButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log("Button clicked") // Check if this appears
+    let formAuthor = document.getElementById('author')
+    let formTitle = document.getElementById('title')
+    let formPage = document.getElementById('pages')
+    let formRead = document.getElementById('read')
+    let readValue = formRead.checked
+    addBookToLibrary(formAuthor.value,formTitle.value,formPage.value,readValue)
+    displayBooks()
+    
+    dialog.close()
+    dialog.classList.remove('show')
+}
+)
